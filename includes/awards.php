2cabd5115ea6f1a4d233f50619ccec07544b394f
Dave Lester <dave@ischool.berkeley.edu>
2012-06-25 10:34:19 -0700

    Update meta boxes for custom awards post type.

--- includes/awards.php
+++ includes/awards.php
@@ -48,7 +48,7 @@ class WPbadger_Award_Schema {
 			'capability_type' => 'post',
 			'has_archive' => false,
 			'hierarchical' => false,
-			'supports' => array( 'title', 'author', 'thumbnail' )
+			'supports' => array( 'editor' )
 		);
 
 		register_post_type( $this->get_post_type_name(), $args );
@@ -93,4 +93,118 @@ class WPbadger_Award_Schema {
 }
 new WPbadger_Award_Schema();
 
+add_action( 'load-post.php', 'wpbadger_awards_meta_boxes_setup' );
+add_action( 'load-post-new.php', 'wpbadger_awards_meta_boxes_setup' );
+
+function wpbadger_awards_meta_boxes_setup() {
+	add_action( 'add_meta_boxes', 'wpbadger_add_award_meta_boxes' );
+	add_action( 'save_post', 'wpbadger_save_award_meta', 10, 2 );
+}
+
+// Create metaboxes for post editor
+function wpbadger_add_award_meta_boxes() {
+
+	add_meta_box(
+		'wpbadger-award-choose-badge',		// Unique ID
+		esc_html__( 'Choose Badge', 'example' ),	// Title
+		'wpbadger_award_choose_badge_meta_box',		// Callback function
+		'award',						// Admin page (or post type)
+		'side',							// Context
+		'default'						// Priority
+	);
+	
+	add_meta_box(
+		'wpbadger-award-email-address',		// Unique ID
+		esc_html__( 'Email Address', 'example' ),	// Title
+		'wpbadger_award_email_address_meta_box',		// Callback function
+		'award',						// Admin page (or post type)
+		'side',							// Context
+		'default'						// Priority
+	);
+}
+
+// Display metaboxes
+function wpbadger_award_choose_badge_meta_box( $object, $box ) { ?>
+
+	<?php wp_nonce_field( basename( __FILE__ ), 'wpbadger_award_nonce' ); ?>
+	
+	<p>
+	<select name="wpbadger_badge_id" id="wpbadger_badge_id">
+	
+	<?php $query = new WP_Query( array( 'post_type' => 'badge' ) );
+	
+	while ( $query->have_posts() ) : $query->the_post();
+		echo '<li>';
+		echo "<option id='wpbadger-award-choose-badge' value='";
+		echo get_post_meta( $object->ID, 'wpbadger-badge-choose-badge', true );
+		echo "'>";
+		echo  the_title() . " (Version X)</option>";
+	endwhile;
+	?>
+	
+	</select>
+	</p>
+<?php }
+
+function wpbadger_award_email_address_meta_box( $object, $box ) { ?>
+
+	<p>
+		<input class="widefat" type="text" name="wpbadger-award-email-address" id="wpbadger-award-email-address" value="<?php echo esc_attr( get_post_meta( $object->ID, 'wpbadger-award-email-address', true ) ); ?>" size="30" />
+	</p>
+<?php }
+
+function wpbadger_save_award_meta( $post_id, $post ) {
+
+	if ( !isset( $_POST['wpbadger_award_nonce'] ) || !wp_verify_nonce( $_POST['wpbadger_award_nonce'], basename( __FILE__ ) ) )
+		return $post_id;
+
+	$post_type = get_post_type_object( $post->post_type );
+
+	if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+		return $post_id;
+
+	$chosen_badge_new_meta_value = $_POST['wpbadger-award-choose-badge'];
+	$chosen_badge_meta_key = 'wpbadger-badge-version';
+	$chosen_badge_meta_value = get_post_meta( $post_id, $meta_key, true );
+
+	$email_new_meta_value = $_POST['wpbadger-award-email-address'];
+	$email_meta_key = 'wpbadger-award-email-address';
+	$email_meta_value = get_post_meta( $post_id, $meta_key, true );
+
+	if ( $chosen_badge_new_meta_value && '' == $chosen_badge_meta_value ) {
+		add_post_meta( $post_id, $chosen_badge_meta_key, $chosen_badge_new_meta_value, true );
+	} elseif ( $chosen_badge_new_meta_value && $chosen_badge_new_meta_value != $chosen_badge_meta_value ) {
+		update_post_meta( $post_id, $chosen_badge_meta_key, $chosen_badge_new_meta_value );
+	} elseif ( '' == $chosen_badge_new_meta_value && $chosen_badge_meta_value ) {
+		delete_post_meta( $post_id, $chosen_badge_meta_key, $chosen_badge_meta_value );
+	}
+	
+	if ( $email_new_meta_value && '' == $email_meta_value ) {
+		add_post_meta( $post_id, $email_meta_key, $email_new_meta_value, true );
+	} elseif ( $email_new_meta_value && $email_new_meta_value != $email_meta_value ) {
+		update_post_meta( $post_id, $email_meta_key, $email_new_meta_value );
+	} elseif ( '' == $email_new_meta_value && $email_meta_value ) {
+		delete_post_meta( $post_id, $email_meta_key, $email_meta_value );	
+	}
+}
+
+add_filter( 'template_include', 'award_template_check' );
+
+function award_template_check() {
+	global $template;
+
+	if (is_single() ){
+		// Get query information
+		$accept = get_query_var( 'accept' );
+
+		// Check if post type 'Awards'
+		if ( 'award' == get_post_type() ) {
+			
+			$template_file = dirname(__FILE__) . '/includes/awards_template.php';
+			return $template_file;
+		}
+	}
+
+	return $template;
+}
 ?>
