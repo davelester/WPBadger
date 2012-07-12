52688320ee270fbadb512020c93c4b5b189a66c4
Dave Lester <davelester@gmail.com>
2012-07-11 23:44:40 -0700

    Only update the award status to accepted if it was previously awarded.

--- includes/awards_accept.php
+++ includes/awards_accept.php
@@ -1,3 +1,6 @@
 <?php
 global $post;
-update_post_meta( $post->ID, 'wpbadger-award-status', 'Accepted');
+// Only update the post meta if the previous status was 'Awarded'
+if (get_post_meta($post->ID, 'wpbadger-award-status', true) == 'Awarded') {
+	update_post_meta($post->ID, 'wpbadger-award-status', 'Accepted');
+}
