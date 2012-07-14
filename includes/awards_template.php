57ad2a881154ce92df4406bad6724b991c37ac0f
Dave Lester <davelester@gmail.com>
2012-07-13 20:08:22 -0700

    Move wpbadger javascript to its own file, cleaning up the awards template.

--- includes/awards_template.php
+++ includes/awards_template.php
@@ -3,54 +3,11 @@ global $post;
 
 wp_enqueue_script('openbadges', 'http://beta.openbadges.org/issuer.js', array(), null);
 wp_enqueue_script('jquery_ajax', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+wp_enqueue_script('wpbadger', plugins_url() . '/WPBadger/includes/wpbadger.js');
 get_header(); ?>
 
 <div id="container">
 	<div id="content" role="main">
-
-<script>
-$(document).ready(function() {
-	// Some js originally based on Badge it Gadget Lite https://github.com/Codery/badge-it-gadget-lite/blob/master/digital-badges/get-my-badge.php
-	
-	$('.js-required').hide();
-	
-	if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){  //The Issuer API isn't supported on MSIE browsers
-		$('.backPackLink').hide();
-		$('.login-info').hide();
-		$('.browserSupport').show();
-	}
-	
-	// Function that issues the badge
-	$('.backPackLink').click(function() {
-		var assertionUrl = "<?php echo get_permalink(); ?>json/";
-		OpenBadges.issue([''+assertionUrl+''], function(errors, successes) {					
-			if (successes.length > 0) {
-					$('.backPackLink').hide();
-					$('.login-info').hide();
-					$('#badgeSuccess').show();
-					$.ajax({
-  						url: '<?php echo get_permalink(); ?>accept/',
-  						type: 'POST',
-						success: function(data, textStatus) {
-							window.location.href = '<?php echo get_permalink(); ?>';
-						}
-					});
-				}
-			});
-		});
-	
-	// Function that rejects the badge
-	$('.rejectBadge').click(function() {
-		$.ajax({
-			url: '<?php echo get_permalink(); ?>reject/',
-			type: 'POST',
-			success: function(data, textStatus) {
-				window.location.href = '<?php echo get_permalink(); ?>';
-			}
-		});
-	});
-});
-</script>
 
 <?php
 $award_status = get_post_meta($post->ID, 'wpbadger-award-status', true);
