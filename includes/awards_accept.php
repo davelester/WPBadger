8249dd318357da33fd2d34b6ea20251ce50ce0f5
Dave Lester <davelester@gmail.com>
2012-07-14 00:10:19 -0700

    Fixes conflict with WP Super Cache plugin by adding a check to see if it's active. If it is, clear the cache for a specific award when its status is updated.

--- includes/awards_accept.php
+++ includes/awards_accept.php
@@ -4,3 +4,11 @@ global $post;
 if (get_post_meta($post->ID, 'wpbadger-award-status', true) == 'Awarded') {
 	update_post_meta($post->ID, 'wpbadger-award-status', 'Accepted');
 }
+
+// Include administrative plugin helpers
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+// If WP Super Cache Plugin installed, delete cache files for award post
+if (is_plugin_active('wp-super-cache/wp-cache.php')) {
+   wp_cache_post_change($post->ID);
+}
